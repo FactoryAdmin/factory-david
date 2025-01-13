e33c6e8bba9f50efe20334576e61759cd3596450
David Aguirre <aguirredavid.92@gmail.com>
2025-01-13 18:01:35 -0300

    pagination done

--- tables/dom/dom.js
+++ tables/dom/dom.js
@@ -1,9 +1,20 @@
-document.addEventListener('DOMContentLoaded', initialData)
+document.addEventListener('DOMContentLoaded', /* initialData */ ()=>{
+    createPagination();
+    setupPaginationEvents();
+    showRecordsPerPage(1);
+});
 
 
-function initialData() {
+function initialData(newArrayData) {
     $tbodyMainTable.innerHTML = '';
-    usersData.forEach((userData) => {
+    /* usersData.forEach((userData) => {
+        const $row = createUserRow(userData);
+        $tbodyMainTable.appendChild($row);
+    }); */
+    /* createPagination();
+    setupPaginationEvents(); */
+
+    newArrayData.forEach((userData) => {
         const $row = createUserRow(userData);
         $tbodyMainTable.appendChild($row);
     });
@@ -19,15 +30,15 @@ function createUserRow(userData) {
 
     const $actionsTd = document.createElement('td');
     $actionsTd.appendChild(generateButton('Eliminar', 'error', () => confirmDeleteItem(userData)));
-    $actionsTd.appendChild(generateButton('Modificar', 'primary', () => {}));
+    $actionsTd.appendChild(generateButton('Modificar', 'primary', () => { }));
 
     $row.appendChild($actionsTd);
     return $row;
 }
 
 
-function generateButton(btnName,classBtn,actionBtn) {
-    
+function generateButton(btnName, classBtn, actionBtn) {
+
     let $button = document.createElement('button');
     $button.textContent = btnName;
     $button.classList.add(btnClasses[classBtn] || DEFAULT_STYLE_BTN);
@@ -66,21 +77,61 @@ function confirmDeleteItem(userData) {
 
 
 function createPagination() {
-    const pagesQuantities = usersData.length / 5;
+    const registerToShow = 10;
+    const pagesQuantities = Math.ceil(usersData.length / registerToShow);
 
-    for (let index = 1; index <= Math.ceil(pagesQuantities); index++) {
+    for (let index = 1; index <= pagesQuantities; index++) {
 
         let $pages = document.createElement('a');
         $pages.setAttribute('href', '#');
+        $pages.setAttribute('data-page-id', index);
         $pages.textContent = index;
         $pages.classList.add('pages-style');
         $paginationContainer.classList.add('pagination-containerStyle');
         $paginationContainer.appendChild($pages);
-        
+
     }
 }
 
+
+
+function setupPaginationEvents() {
+    const links = document.querySelectorAll('#pagination-container > a');
+
+    for (let index = 0; index < links.length; index++) {
+        const currentPage = links[index].getAttribute('data-page-id');
+        links[index].addEventListener('click',()=> showRecordsPerPage(currentPage));
+    }
+};
+
+
+function showRecordsPerPage(currentPage) {
+    const size = 10;
+    const lastIndex = currentPage * size;
+    const firstIndex = lastIndex - size;
+    const newArrayData = usersData.slice(firstIndex, lastIndex);
+
+
+    
+    initialData(newArrayData);
+    activePage(currentPage);
+}
+
+function activePage(currentPage) {
+    const links = document.querySelectorAll('#pagination-container > a');
+
+    links.forEach(page=> {
+        page.classList.remove('activePage');
+    })
+    
+    const activeLink = document.querySelector(`#pagination-container > a:nth-child(${currentPage})`);
+    activeLink.classList.add('activePage');
+    
+}
+
+
 setupModalEvents();
-createPagination();
+
+
 
 
